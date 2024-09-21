cf142c49da552c701b9bc53cdf99234020b81411
Ivan Molto <ivanmolto@gmail.com>
2024-09-21 23:20:44 +0200

    chore: Add navigation and sections

---
+++ src/lib/navigation.ts
@@ -0,0 +1,26 @@
+export const navigation = [
+  {
+    title: 'WTF is PSBT?',
+    links: [
+      { title: 'For Nerds', href: '/' },
+      { title: 'ELI5', href: '/docs/eli5' },
+      { title: 'Motivation', href: '/docs/motivation' },
+    ],
+  },
+  {
+    title: 'Code and Documentation',
+    links: [
+      { title: 'Updates and Contributions', href: '/docs/updates' },
+      { title: 'BIP-174', href: '/docs/bip-174' },
+      { title: 'BIP-370', href: '/docs/bip-370' },
+    ],
+  },
+  {
+    title: 'More resources',
+    links: [
+      { title: 'Memes', href: '/docs/memes' },
+      { title: 'Quick Glossary', href: '/docs/quick-glossary' },
+      { title: 'External Links', href: '/docs/external-links' },
+    ],
+  },
+]
